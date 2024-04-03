c817b7b8990e00b10029ba4472398a58aa64a6ea
ajzelaya <ajzelaya@banhcafe.hn>
2024-04-03 16:26:45 -0600

    Agregar BlogContentPage

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,67 +1,41 @@
-
 const Blogs = () => {
   const blogs = [
     {
       id: 1,
-      title: "Blog1",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nam consectetur distinctio!",
-      coverImg: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
+      title: "Blog 1",
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae",
+      cover: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
     },
     {
-      id: 1,
-      title: "Blog1",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nam consectetur distinctio!",
-      coverImg: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
+      id: 2,
+      title: "Blog 2",
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae",
+      cover: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
     },
     {
-      id: 1,
-      title: "Blog1",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nam consectetur distinctio!",
-      coverImg: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
+      id: 3,
+      title: "Blog 3",
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae",
+      cover: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
     },
   ];
   return (
     <div className="w-full py-32">
       <div className="max-w-[1240px] mx-auto">
-        <div className="grid lg:grid-cols-3 gap-8 px-4 text-black">
-
-
-          <div className="bg-white rounded-xl overflow-hidden drop-shadow-md">
-            <img
-              className="h-56 w-full object-cover"
-              src="https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75"
-              alt="blogImage"
-            />
-            <div className="p-8">
-              <h3 className="font-bold text-2xl my-1">Blog 1</h3>
-              <p className="text-gray-600 text-xl">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt blanditiis facere pariatur ea magnam
-                obcaecati earum vitae vero consectetur error, quas consequuntur voluptatem porro unde officia soluta
-                delectus debitis odio!
-              </p>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 ss:grid-cols-1 gap-8 px-4 text-black">
+          {blogs.map((blog) => (
+            <div key={blog.id} className="bg-white rounded-xl overflow-hidden drop-shadow-md">
+              <img className="h-56 w-full object-cover" src={blog.cover} alt="" />
+              <div className="p-8">
+                <h3 className="font-bold text-2xl my-1">{blog.title}</h3>
+                <p className="text-gray-600 text-lg md:text-base">{blog.desc}</p>
+              </div>
             </div>
-          </div>
-
-          <div className="bg-white rounded-xl overflow-hidden drop-shadow-md">
-            <img
-              className="h-56 w-full object-cover"
-              src="https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75"
-              alt="blogImage"
-            />
-            <div className="p-8">
-              <h3 className="font-bold text-2xl my-1">Blog 1</h3>
-              <p className="text-gray-600 text-xl">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt blanditiis facere pariatur ea magnam
-                obcaecati earum vitae vero consectetur error, quas consequuntur voluptatem porro unde officia soluta
-                delectus debitis odio!
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Blogs;
