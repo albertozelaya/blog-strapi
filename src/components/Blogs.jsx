c2dbecb6c23b2049a917287ef4014417de09d3b0
ajzelaya <ajzelaya@banhcafe.hn>
2024-04-15 14:48:17 -0600

    conrimacion

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,36 +1,5 @@
 import { Link } from "react-router-dom";
 const Blogs = ({blogs}) => {
-  //   {
-  //     id: 1,
-  //     title: "Blog 1",
-  //     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae",
-  //     cover: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
-  //     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dignissimos iure inventore.",
-  //     authorName: "John Doe",
-  //     authorImg: "https://img.freepik.com/free-photo/smiling-redhead-man-with-laptop-looking-camera-cafe_1163-5162.jpg",
-  //     authorDesc: "Web Developer",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Blog 2",
-  //     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae",
-  //     cover: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
-  //     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dignissimos iure inventore.",
-  //     authorName: "John Doe",
-  //     authorImg: "https://img.freepik.com/free-photo/smiling-redhead-man-with-laptop-looking-camera-cafe_1163-5162.jpg",
-  //     authorDesc: "Web Developer",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Blog 3",
-  //     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae",
-  //     cover: "https://www.banhcafe.hn/_next/image?url=%2Fannouncement-2.webp&w=640&q=75",
-  //     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dignissimos iure inventore.",
-  //     authorName: "John Doe",
-  //     authorImg: "https://img.freepik.com/free-photo/smiling-redhead-man-with-laptop-looking-camera-cafe_1163-5162.jpg",
-  //     authorDesc: "Web Developer",
-  //   },
-  // ];
   return (
     <div className="w-full py-32">
       <div className="max-w-[1240px] mx-auto">
